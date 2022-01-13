e984342f54f5713bdae192580b65d02f99f3544a
Ryan Garren <rryangarren@gmail.com>
2022-01-13 14:46:34 -0700

    rendered menu icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./assets/App.css";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>{"Hello World"}</div>
+        <Header />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+export default function Header() {
+  return(
+  <div>
+    <span class="material-icons orange unique">menu</span>
+    <div></div>
+    <div></div>
+  </div>
+  )
+  }
