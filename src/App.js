778cee67314e1a3e6235769672debe6514a808f3
Ryan Garren <rryangarren@gmail.com>
2022-01-13 15:31:46 -0700

    added inputs on app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./assets/App.css";
 import Header from "./components/Header/Header";
+import AddLift from './components/AddLift/AddLift'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <AddLift />
       </div>
     )
   }
--- src/components/AddLift/AddLift.js
+++ src/components/AddLift/AddLift.js
@@ -1,1 +1,26 @@
+import React, { Component } from 'react'
 import './AddLift.css'
+
+class AddLift extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div className='addlift-parent'>
+        <div>New Lifting Entry:</div>
+        <input placeholder='Enter Name of Lift'></input>
+        <input></input>
+        <input></input>
+        <input></input>
+      </div>
+    )
+  }
+}
+
+export default AddLift
